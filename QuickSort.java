4e4b1658cd44e86f74a7be9d1742817300faef51
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2024-10-07 22:17:27 -0400

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,9 +1,13 @@
 package QuickSort;
 
 public class QuickSort {
-    public static void quicksort(int[] array, int lo, int hi) {
+    public static void quickSort(int[] array, int lo, int hi) {
         if (lo >= hi || lo < 0) {
             return;
         }
+        int p = partition(array, lo, hi);
+        quickSort(array, lo, p - 1);
+        quickSort(array, p + 1, hi);
     }
+    
 }
