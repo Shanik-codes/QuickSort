c62dfb715d821b14ff617147151b912e6f11d4e3
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2024-10-07 22:14:55 -0400

    Update QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,9 @@
+package QuickSort;
+
+public class QuickSort {
+    public static void quicksort(int[] array, int lo, int hi) {
+        if (lo >= hi || lo < 0) {
+            return;
+        }
+    }
+}
